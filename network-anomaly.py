a234adab77fe0fe5312fba32d8217b877cad43bb
jay1-53 <steven.daanton@gmail.com>
2021-10-04 04:40:25 +0800

    Fix changes for sent and receive network bytes

--- network-anomaly.py
+++ network-anomaly.py
@@ -27,9 +27,20 @@ def log_cpu(timestampStr, cpu_usage):
 
 def data_gather(timestampStr):
     # initial_process_count = 0
-    network_usage = []
+    network_receive_usage = []
+    network_sent_usage = []
     cpu_usage = []
     storage_usage = []
+    NETWORK_INTERFACE = 'Ethernet'
+    netio = psutil.net_io_counters(pernic=True)
+
+    receive_net_usage = netio[NETWORK_INTERFACE].bytes_recv
+    temporary_receive_value = receive_net_usage
+    bytes_received = 0
+
+    sent_net_usage = netio[NETWORK_INTERFACE].bytes_sent
+    temporary_sent_value = sent_net_usage
+    bytes_sent = 0
     while True:
         current_time = time.time()
         elapsed_time = current_time - start_time
@@ -37,34 +48,34 @@ def data_gather(timestampStr):
         # process_count = 0
         # for proc in psutil.process_iter(): initial_process_count += 1
         ### ----------------------------------------
-        NETWORK_INTERFACE = 'Ethernet'
-        netio = psutil.net_io_counters(pernic=True)
-        net_usage = netio[NETWORK_INTERFACE].bytes_recv
-        temporary_network_value = net_usage
-        bytes_received = 0
 
         # NETWORK_LIMIT = net_usage + 5368709120
         
         ### CPU Statistics
         cpu_usage_percentage = psutil.cpu_percent()
+        print(cpu_usage_percentage)
         cpu_usage.append(cpu_usage_percentage)
         ### Disk usage statistics
         storage_usage_stats = psutil.disk_usage('/')
+        print(storage_usage_stats)
         storage_usage.append(storage_usage_stats)
         ### Network Receive
         netio = psutil.net_io_counters(pernic=True)
-        net_usage = netio[NETWORK_INTERFACE].bytes_recv
-
+        receive_net_usage = netio[NETWORK_INTERFACE].bytes_recv
+        bytes_received = receive_net_usage - temporary_receive_value
+        temporary_receive_value = receive_net_usage
+        print(bytes_received)
+        ### Network sent
+        netio = psutil.net_io_counters(pernic=True)
+        sent_net_usage = netio[NETWORK_INTERFACE].bytes_sent
+        bytes_sent = sent_net_usage - temporary_sent_value
+        temporary_sent_value = sent_net_usage
+        print(bytes_sent)
         ###---FOR NETWORK LIMITING---
         # if net_usage > NETWORK_LIMIT:
         #     print("Meets network limit!")
         #     more process here
         ###--------------------------
-
-        ###
-        bytes_received = net_usage - temporary_network_value
-        temporary_network_value = net_usage
-        network_usage.append(bytes_received)
 
         ###---PROCESS LIST GATHERING---
         # for proc in psutil.process_iter(): process_count += 1
@@ -85,9 +96,8 @@ def data_gather(timestampStr):
         ###------------------------------
 
         if elapsed_time > seconds:
-            print(timestampStr)
-            log_network_receive(timestampStr, network_usage)
-            log_network_sent(timestampStr, network_usage)
+            log_network_receive(timestampStr, network_receive_usage)
+            log_network_sent(timestampStr, network_sent_usage)
             log_storage(timestampStr, storage_usage)
             log_cpu(timestampStr, cpu_usage)
             break
